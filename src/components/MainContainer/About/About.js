d7ccad0d65636abc2d664d99c3296eb689c154d3
ishumski <whalterwhite23@gmail.com>
2021-04-15 12:24:09 +0300

    Add about section

--- src/components/MainContainer/About/About.js
+++ src/components/MainContainer/About/About.js
@@ -4,7 +4,18 @@ import "./About.css";
 function About() {
     return (
         <div className="about">
-            <h2>About</h2>
+            <img
+                className="about__profile-photo"
+                src="https://data.whicdn.com/images/20433362/original.jpg"
+                alt="profile-photo"
+            />
+            <div className="about__links">
+
+                <a href="https://www.google.com/">google</a>
+                <a href="https://twitter.com/">twitter</a>
+                <a href="https://www.youtube.com/">youtube</a>
+                <p>Profile</p>
+            </div>
         </div>
     )
 }
