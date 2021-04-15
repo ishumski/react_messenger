ce269d139a42840cb53f3705933c4c10ff96f38c
ishumski <whalterwhite23@gmail.com>
2021-04-15 13:23:58 +0300

    Rename folder container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Container.css";
+import About from "./About/About.js";
+import Sidebar from "./Sidebar/Sidebar.js";
+import Main from "./Main/Main.js";
+
+
+
+
+function Container() {
+    return (
+
+        <div className="container">
+            <About />
+            <div className="container__main">
+                <Sidebar />
+                <Main />
+            </div>
+        </div>
+
+    )
+}
+export default Container;
