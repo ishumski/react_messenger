6863b83d54223785ce781897a89627bda04b10a6
ishumski <whalterwhite23@gmail.com>
2021-04-30 16:03:37 +0300

    Add post functionality

--- src/components/Container/Pages/Timeline/TimelineBox/TimelineBox.js
+++ src/components/Container/Pages/Timeline/TimelineBox/TimelineBox.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TimelineBox.css";
 import { Avatar, Button } from '@material-ui/core';
+import db from "../../../../../firebase.js"
 
 
 
 function TimelineBox() {
+
+    const [publishPost, setPublishPost] = useState("");
+
+    const addPost = (event) => {
+        event.preventDefault();
+
+        db.collection("posts").add(({
+            displayName: "Jane Doy",
+            username: "deadinside",
+            text: publishPost,
+            avatar: 'https://data.whicdn.com/images/20433362/original.jpg',
+            image: 'https://memax.club/wp-content/uploads/2019/05/55-176.jpg'
+        }))
+        setPublishPost("");
+    }
+
     return (
         <div className="timeline__box">
             <form>
                 <div className="timeline__box_input">
-                    <Avatar />
-                    <input type="text" placeholder="What's happening..." />
+                    <Avatar src='https://data.whicdn.com/images/20433362/original.jpg' />
+                    <input
+                        type="text"
+                        placeholder="What's happening..."
+                        value={publishPost}
+                        onChange={(event) => setPublishPost(event.target.value)}
+
+                    />
                 </div>
-                <Button className="timeline__btn">Post</Button>
+                <Button
+                    className="timeline__btn"
+                    type="submit"
+                    onClick={addPost}
+
+                >
+                    Post
+                </Button>
             </form>
         </div>
     )
