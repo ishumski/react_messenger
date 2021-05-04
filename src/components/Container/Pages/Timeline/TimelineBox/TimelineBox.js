ba6b5ffa4fd390f57739eeffaf58a2adc4fd1627
ishumski <whalterwhite23@gmail.com>
2021-05-04 15:07:56 +0300

    Add post functionality

--- src/components/Container/Pages/Timeline/TimelineBox/TimelineBox.js
+++ src/components/Container/Pages/Timeline/TimelineBox/TimelineBox.js
@@ -1,51 +1,74 @@
 import React, { useState } from 'react';
 import "./TimelineBox.css";
 import { Avatar, Button } from '@material-ui/core';
-import { db } from "../../../../../firebase.js"
-import ImageUpload from '../../../../../ImageUpload';
-
-
+import { db, storage } from "../../../../../firebase.js"
 
 function TimelineBox() {
 
-    const [publishPost, setPublishPost] = useState("");
+    const [caption, setCaption] = useState("");
+    const [image, setImage] = useState(null);
 
-    const addPost = (event) => {
-        event.preventDefault();
+    //выбираем картинку
+    const handleChange = (event) => {
+        if (event.target.files[0]) {
+            setImage(event.target.files[0])
+        }
+    }
 
-        db.collection("posts").add(({
-            displayName: "Jane Doy",
-            username: "deadinside",
-            text: publishPost,
-            avatar: 'https://data.whicdn.com/images/20433362/original.jpg',
-            image: 'https://memax.club/wp-content/uploads/2019/05/55-176.jpg'
-        }))
-        setPublishPost("");
+    //ссылка на файл images, где будут все наши картинки
+    const handleUpload = (event) => {
+
+        event.preventDefault()
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => { },
+            (error) => {
+                alert(error.message);
+            },
+            () => {
+                storage
+                    .ref("images")
+                    .child(image.name)
+                    .getDownloadURL()
+                    .then((url) => {
+                        db.collection("posts").add({
+                            displayName: "Jane Doe",
+                            username: "deadinside",
+                            text: caption,
+                            avatar: 'https://data.whicdn.com/images/20433362/original.jpg',
+                            image: url,
+                        });
+
+                        setCaption("");
+                        setImage(null);
+                    });
+            })
+        console.log(image)
     }
 
     return (
-        <div className="timeline__box">
+        <div className="timeline__box" >
             <form>
                 <div className="timeline__box_input">
                     <Avatar src='https://data.whicdn.com/images/20433362/original.jpg' />
                     <input
                         type="text"
                         placeholder="What's happening..."
-                        value={publishPost}
-                        onChange={(event) => setPublishPost(event.target.value)}
+                        value={caption}
+                        onChange={(event) => setCaption(event.target.value)}
 
                     />
                 </div>
-                <ImageUpload />
+                <input type="file" onChange={handleChange} />
                 <Button
                     className="timeline__btn"
                     type="submit"
-                    onClick={addPost}
-
+                    onClick={handleUpload}
                 >
                     Post
                 </Button>
-                
             </form>
         </div>
     )
